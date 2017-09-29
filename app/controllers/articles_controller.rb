e20020f2a94291e9519d9be2a4cdc899dfce385c
Connor <connor.alcock.87@gmail.com>
2017-09-29 14:15:34 -0600

    Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
   end
 
