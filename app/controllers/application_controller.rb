5450567881a0cd61dcc11d9a02106210b4b92172
Connor <connor.alcock.87@gmail.com>
2017-09-29 13:47:58 -0600

    Posting to db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+    render plain: params[:article].inspect
+  end
+end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
   end
 end
