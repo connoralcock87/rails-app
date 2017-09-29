cc6511631219eb67c778f1aa306cc3add3589b74
Connor <connor.alcock.87@gmail.com>
2017-09-29 16:45:33 -0600

    Updated error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,26 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
