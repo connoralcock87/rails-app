d728c9bc4e8873b22b05437330d90139f28fca26
Connor <connor.alcock.87@gmail.com>
2017-10-01 02:04:39 -0600

    Fixed controller bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  
   def index
     @articles = Article.all
   end
@@ -34,7 +35,14 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
