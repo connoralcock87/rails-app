2800043bd50af6840fbb02d7cebcb02360078ff7
Connor <connor.alcock.87@gmail.com>
2017-09-30 23:51:20 -0600

    added update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -20,6 +24,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
